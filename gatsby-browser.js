f8778abc7f23f33561b81fe94ec217ce287fdb08
Fredrik S <fredrik.spannar@gmail.com>
2023-07-02 18:26:11 +0200

    Added Tailwind CSS

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -3,7 +3,10 @@ import "typeface-montserrat"
 import "typeface-merriweather"
 
 // normalize CSS across browsers
-import "./src/css/normalize.css"
+//import "./src/css/normalize.css"
 
 // custom CSS styles
-import "./src/css/style.css"
+//import "./src/css/style.css"
+
+// get tailwind global
+import './src/css/global.css'
