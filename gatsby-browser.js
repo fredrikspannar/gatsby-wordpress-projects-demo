52caa80b76797032027f9f25e23460abe632d380
Fredrik S <fredrik.spannar@gmail.com>
2023-07-03 09:35:06 +0200

    Some clean-up of dependencies not used

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,12 +1,5 @@
-// custom typefaces
-import "typeface-montserrat"
-import "typeface-merriweather"
-
 // normalize CSS across browsers
-//import "./src/css/normalize.css"
-
-// custom CSS styles
-//import "./src/css/style.css"
+import "./src/css/normalize.css"
 
 // get tailwind global
 import './src/css/global.css'
