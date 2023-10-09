46e5025e46860e5f30130deb1aadb3bfb3a7d472
Fredrik S <fredrik.spannar@gmail.com>
2023-10-09 20:36:46 +0200

    Added tags (shown as tech)

--- src/components/projectposts.js
+++ src/components/projectposts.js
@@ -28,7 +28,12 @@ const ProjectPosts = () => {
                                 nodes {
                                     name
                                 }  
-                            }                            
+                            }
+                            tags {
+                                nodes {
+                                  name
+                                }
+                              }                                                        
                         }
                     }
                 }                
@@ -46,9 +51,14 @@ const ProjectPosts = () => {
                 const id = item?.node?.id || index;
                 const slug = item?.node?.slug || "";
                 let categories = [];
+                let tags = [];
 
                 if ( item && item.node && item.node.categories && item.node.categories.nodes ) {
                     categories = item.node.categories.nodes.map(itemCategory => (itemCategory.name));
+                }
+
+                if ( item && item.node && item.node.tags && item.node.tags.nodes ) {
+                    tags = item.node.tags.nodes.map(itemTag => (itemTag.name));
                 }
 
                 return (
@@ -56,11 +66,20 @@ const ProjectPosts = () => {
                       <div className="p-2 mb:p-4 flex flex-col md:flex-row justify-between mt-4 mb-8 rounded-lg border-2 bg-slate-400 border-slate-400 hard-shadow-10px" key={id}>
                         <div className="project-content">
                             <div className="font-bold text-lg mb-2">{parse(title)}</div>
-                            {categories.length > 0 && (
-                                <div className="mb-4 mt-2">
-                                    {categories.map(category => <span className="px-2 py-1 bg-slate-500 text-white rounded-lg">Category: {category}</span>)}
-                                </div>
-                            )}
+                            <div className="flex">
+                                {categories.length > 0 && (
+                                    <div className="mb-4 mt-2">
+                                        Categor{categories.length == 1 ? "y" : "ies"}: 
+                                        {categories.map((category, index) => <span className="px-2 py-1 ml-1 bg-slate-500 text-white rounded-lg" key={`category-${index}`}>{category}</span>)}
+                                    </div>
+                                )}
+                                {tags.length > 0 && (
+                                    <div className="mb-4 mt-2 ml-4">
+                                        Tech: 
+                                        {tags.map((tag, index) => <span className="px-2 py-1 ml-1 bg-stone-600 text-white rounded-lg" key={`category-${index}`} >{tag}</span>)}
+                                    </div>
+                                )}
+                            </div>
                             <div className="italic project-excerpt">{parse(excerpt)}</div>
                             <div className="mt-6 mb-6"><Link className="bg-yellow-700 py-3 px-6 text-white rounded-lg hard-shadow-6px hover:opacity-60" to={`/${slug}`}>Read more &gt;</Link ></div>
                         </div>
